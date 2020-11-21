ddeaece8f89f44a3e5dbd87fe258a66a1584ec1a
Akash Bhardwaj <akashbwj@gmail.com>
2020-11-21 20:46:39 +0530

    modify app.js Port

--- app.js
+++ app.js
@@ -65,6 +65,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
 
-app.listen(3000,function(){
+app.listen(process.env.PORT||3000,process.env.IP,function(){
 	console.log("The YelpCamp Server Has Started Listening!!!")
 });
